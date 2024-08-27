29878777734ad9b289e89002216cb89aa7a9a136
clcment446 <125682659+clcment446@users.noreply.github.com>
2024-08-27 15:01:55 +0200

    unstable, can not compile.
    need to comment IronBoundCoreEntities#clinit out, and some other things...

--- src/main/java/com/c446/ironbound_core/registry/IronboundCoreEntities.java
+++ src/main/java/com/c446/ironbound_core/registry/IronboundCoreEntities.java
@@ -11,31 +11,30 @@ import net.minecraft.world.entity.MobCategory;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
+import net.minecraft.world.entity.EntityType.Builder;
 
 public class IronboundCoreEntities {
     public static final DeferredRegister<EntityType<?>> ENTITIES = DeferredRegister.create(ForgeRegistries.ENTITY_TYPES, IronBound.MOD_ID);
-//    public static final RegistryObject<EntityType<Entity>> MOONLIGHT_RAY_ENTITY;
-//
-//    private static final RegistryObject<EntityType<Entity>> BLOOD_SLASH_PROJECTILE;
-//
-//    static {
-//        ENTITIES = ;
-//
-//        MOONLIGHT_RAY_ENTITY = ENTITIES.register("moonlight_ray", () -> {
-//            return EntityType.Builder.of(MoonlightRayEntity::new, MobCategory.MISC)
-//                    .sized(2.0F, 0.5F)
-//                    .clientTrackingRange(64)
-//                    .build(
-//                            new ResourceLocation("irons_spellbooks", "moonlight_ray").toString());
-//        });
-//
-//        BLOOD_SLASH_PROJECTILE = ENTITIES.register("blood_slash", () -> {
-//            return EntityType.Builder.of(BloodSlashProjectile::new, MobCategory.MISC).sized(2.0F, 0.5F).clientTrackingRange(64).build((new ResourceLocation("irons_spellbooks", "blood_slash")).toString());
-//        });
-//    }
+    public static final RegistryObject<EntityType<Entity>> MOONLIGHT_RAY_ENTITY;
+
+    private static final RegistryObject<EntityType<Entity>> BLOOD_SLASH_PROJECTILE;
+
+    static {
+
+        MOONLIGHT_RAY_ENTITY = ENTITIES.register("moonlight_ray", () -> {
+            return Builder.of(MoonlightRayEntity::new, MobCategory.MISC)
+                    .sized(2.0F, 0.5F)
+                    .clientTrackingRange(64)
+                    .build((new ResourceLocation(IronBound.MOD_ID, "moonlight_ray")).toString());
+        });
+
+        BLOOD_SLASH_PROJECTILE = ENTITIES.register("blood_slash", () -> {
+            return Builder.of(BloodSlashProjectile::new, MobCategory.MISC).sized(2.0F, 0.5F).clientTrackingRange(64).build((new ResourceLocation("irons_spellbooks", "blood_slash")).toString());
+        });
+    }
 
     public static <T extends Entity> EntityType.Builder<T> createBuilder(EntityType.EntityFactory<T> factory, MobCategory category) {
-        return EntityType.Builder.of(factory, category);
+        return Builder.of(factory, category);
     }
 
     public static <T extends Entity> RegistryObject<EntityType<T>> registerEntity(String name, EntityType.Builder<T> builder) {
